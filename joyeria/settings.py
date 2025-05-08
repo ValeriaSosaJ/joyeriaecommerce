6be27d18d3dfd98c11939eb1b62e1a3deeb7cb1b
ValeriaSosaJuarez-222761 <al222761@alumnos.uacj.mx>
2025-05-08 14:21:35 -0600

    Agregar whitenoise

--- joyeria/settings.py
+++ joyeria/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,5 +132,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = 'home_ecommerce'
 LOGOUT_REDIRECT_URL = 'home_ecommerce'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
